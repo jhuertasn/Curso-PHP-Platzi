91209fb8a6e0c3e025f8d40204bf26ba5768c8b6
jhuertasn <jhuertasn125@gmail.com>
2023-06-16 18:13:59 -0500

    Introduccion a laravel 9

---
+++ Curso de Introduccion a laravel 9/proyecto/app/Http/Controllers/PageController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function home()
+    {
+        return view('home');
+    }
+
+    public function blog()
+    {
+  //consulta a base de datos
+    //$posts=Post::get();
+    //$post = Post::first();
+    //$post = Post::find(25);
+    //dd($post);
+    $posts=Post::paginate();
+    return view('blog', ['posts'=>$posts]);
+    }
+
+    public function post(Post $post)
+    {
+    //consulta a base de datos
+
+    return view('post',['post' =>$post]);
+    }
+}
