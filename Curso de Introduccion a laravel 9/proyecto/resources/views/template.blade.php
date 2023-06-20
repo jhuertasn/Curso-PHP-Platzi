8f669fb56791d310d46c0605f05ac4a7a1f5f499
jhuertasn <jhuertasn125@gmail.com>
2023-06-20 18:50:17 -0500

    actualizacion de curso laravel 9

--- Curso de Introduccion a laravel 9/proyecto/resources/views/template.blade.php
+++ Curso de Introduccion a laravel 9/proyecto/resources/views/template.blade.php
@@ -5,15 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Proyecto Web</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
-    <p>
-        <a href="{{route('home')}}">Home</a>
-        <a href="{{route('blog')}}">Blog</a>
-    </p>
 
-    <hr>
+    <div class="container px-4 mx-auto">
+        <header class="flex justify-between items-center py-4">
+            <div class="flex items-center flex-grow gap-4">
+                <a href="{{route('home')}}">
+                    <img src="{{asset('images/elefante.png')}}" class="h-12">
+                </a>
+                <form action="">
+                    <input type="text" placeholder="Buscar">
+                </form>
+            </div>
+            @auth 
+                <a href="{{route ('dashboard')}}">Deshboard</a>
+            @else 
+                <a href="{{route ('login')}}">Login</a>   
+            @endauth
 
-    @yield('content')
+        </header>
+        @yield('content')
+    </div>
 </body>
 </html>
--- Curso de Introduccion a laravel 9/proyecto/routes/web.php
+++ Curso de Introduccion a laravel 9/proyecto/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,6 +29,10 @@ Route::controller(PageController::class)->group(function(){
 });
 
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
+Route::resource('posts',PostController::class)->except(['show']);
 
-
+require __DIR__.'/auth.php';
