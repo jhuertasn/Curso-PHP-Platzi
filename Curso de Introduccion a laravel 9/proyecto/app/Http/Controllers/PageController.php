b0d6dfd8c19c1013262e7c3bb4bd572ed5f575f1
jhuertasn <jhuertasn125@gmail.com>
2023-06-21 16:18:37 -0500

    actualizacion ultima de laravel 9

--- Curso de Introduccion a laravel 9/proyecto/app/Http/Controllers/PageController.php
+++ Curso de Introduccion a laravel 9/proyecto/app/Http/Controllers/PageController.php
@@ -7,21 +7,28 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    public function home()
+    public function home(Request $request)
     {
-        return view('home');
+        $search =$request->search;
+        //$posts=Post::latest()->paginate();
+        //return view('home');
+        // se le quitan los parentisi al %($search)% para que pueda funcionar
+        $posts=Post::where('title','LIKE',"%$search%")
+        ->with('user')
+        ->paginate();
+        return view('home', ['posts'=>$posts]);
     }
 
-    public function blog()
-    {
+    //public function blog()
+    //{
   //consulta a base de datos
     //$posts=Post::get();
     //$post = Post::first();
     //$post = Post::find(25);
     //dd($post);
-    $posts=Post::paginate();
-    return view('blog', ['posts'=>$posts]);
-    }
+    //$posts=Post::paginate();
+    //return view('blog', ['posts'=>$posts]);
+    //}
 
     public function post(Post $post)
     {
--- Curso de Introduccion a laravel 9/proyecto/resources/views/template.blade.php
+++ Curso de Introduccion a laravel 9/proyecto/resources/views/template.blade.php
@@ -15,18 +15,35 @@
                 <a href="{{route('home')}}">
                     <img src="{{asset('images/elefante.png')}}" class="h-12">
                 </a>
-                <form action="">
-                    <input type="text" placeholder="Buscar">
+                <form action="{{route('home')}}" method="GET" class="flex-grow">
+                    <input type="text"
+                    name="search" placeholder="Buscar" value="{{request('search')}}" class="border border-gray-200 rounded py-2 px-4 w-1/2">
                 </form>
             </div>
             @auth 
-                <a href="{{route ('dashboard')}}">Deshboard</a>
+                <a href="{{route ('dashboard')}}">Dashboard</a>
             @else 
                 <a href="{{route ('login')}}">Login</a>   
             @endauth
 
         </header>
+
+        <div class="opacity-60 h-px mb-8" style="
+            background: linear-gradient(to right,
+                rgba(200,200,200,0) 0%,
+                rgba(200,200,200,1) 30%,
+                rgba(200,200,200,1) 70%,
+                rgba(200,200,200,0) 100%
+            );
+        ">
+
+        </div>
+
         @yield('content')
+
+        <p class="py-16">
+            <img src="{{asset('images/elefante.png')}}" class="h-12 mx-auto">
+        </p>
     </div>
 </body>
 </html>
